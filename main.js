31cb88d3f8849facd027209a2307cd457cbfbc8e
uwituzeb <b.uwituze@alustudent.com>
2023-11-30 18:51:50 +0200

    fix errors

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ const weatherImg = document.querySelector('.weather-img');
 const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
 
+const locationNotFound = document.querySelector('.location-not-found');
+const weatherBody = document.querySelector('.weather-body');
+
 async function getWeatherData(city) {
     const api = '8743a919f595a49c75d5dd981115a68a';
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`;
     const weatherData = await fetch(URL).then (response => response.json());
 
-    const locationNotFound = document.querySelector('.location-not-found');
-    const weatherBody = document.querySelector('.weather-body');
-    if (weatherData.cod === '404') {
+
+    if (weatherData.cod === `404`) {
         locationNotFound.style.display = "block";
         weatherBody.style.display = "none";
+        return;
     }
 
     locationNotFound.style.display = "none";
